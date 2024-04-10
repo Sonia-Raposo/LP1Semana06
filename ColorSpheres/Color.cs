e8c267c83b9495e85aceebbd78d5d3c30f8f1342
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-10 23:01:34 +0100

    Created methods for Setters and Getters

--- ColorSpheres/Color.cs
+++ ColorSpheres/Color.cs
@@ -1,10 +1,8 @@
-using System;
-
 namespace ColorSpheres
 {
     public class Color
     {
-        private byte red, green, blue, alpha;
+        private byte red, green, blue, alpha, average;
 
 
         public Color(byte r, byte g, byte b, byte a)
@@ -15,6 +13,53 @@ namespace ColorSpheres
             alpha = SetAlpha(a);
         }
 
+        //Set
         
+        private readonly byte SetRed(byte r)
+        {
+            return r;
+        }
+
+        private readonly byte SetGreen(byte g)
+        {
+            return g;
+        }
+
+        private readonly byte SetBlue(byte b)
+        {
+            return b;
+        }
+
+        private readonly byte SetAlpha(byte a)
+        {
+            return a;
+        }
+
+        //Get
+
+        private readonly byte GetRed()
+        {
+            return red;
+        }
+
+        private readonly byte GetGreen()
+        {
+            return green;
+        }
+
+        private readonly byte GetBlue()
+        {
+            return blue;
+        }
+
+        private readonly byte GetAlpha()
+        {
+            return alpha;
+        }
+
+        private int GetGrey()
+        {
+            return (red + green + blue) / 3;
+        }
     }
 }
