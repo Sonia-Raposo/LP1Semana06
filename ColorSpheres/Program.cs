1777c7f84298c4ef54d6b995598777eaa0441337
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-11 21:43:30 +0100

    Creating Colors and Spheres

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -6,7 +6,14 @@ namespace ColorSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creating colors
+            Color violet = new Color (84, 88, 204, 255);
+            Color aqua = new Color (91, 250, 187, 255);
+
+            // Creating spheres
+            Sphere violetSphere = new Sphere (violet, 2.5f);
+            Sphere aquaSphere = new Sphere (aqua, 5.0f);
+  
         }
     }
 }
