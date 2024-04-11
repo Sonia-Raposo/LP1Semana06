19846c502454b1a1cc62ea055d86c0e798b9b269
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-11 20:53:54 +0100

    Added method Pop and method Throw

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -2,11 +2,30 @@ namespace ColorSpheres
 {
     public class Sphere
     {
+        private readonly Color _color;
+        private float _radius;
+        private int _xThrown;
 
-        public Sphere()
+        
+        public Sphere(Color color, float radius)
         {
-            
+            _color = color;
+            _radius = radius;
+            _xThrown = 0;
         }
 
+        public void Pop()
+        {
+            _radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (_radius > 0)
+            {
+                _xThrown++;
+            }
+                
+        }
     }
 }
