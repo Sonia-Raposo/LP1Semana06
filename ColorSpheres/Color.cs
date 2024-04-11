b9f2656a39a08c0ddbaf1dde9c7774608478d130
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-11 22:14:15 +0100

    Added XML documentation

--- ColorSpheres/Color.cs
+++ ColorSpheres/Color.cs
@@ -2,9 +2,20 @@ namespace ColorSpheres
 {
     public class Color
     {
+        /// <summary>
+        /// Setting the variables being used which are readonly
+        /// </summary>
         private readonly byte _red, _green, _blue, _alpha;
 
 
+        /// <summary>
+        /// Method consists in getting colors from the rgb colors (red, green, 
+        /// blue) and transparency (alpha)
+        /// </summary>
+        /// <param name="r">stands for "red"</param>
+        /// <param name="g">stands for "green"</param>
+        /// <param name="b">stands for "blue"</param>
+        /// <param name="a">stands for "alpha"</param>
         public Color(byte r, byte g, byte b, byte a)
         {
             _red = SetRed(r);
@@ -13,6 +24,13 @@ namespace ColorSpheres
             _alpha = SetAlpha(a);
         }
 
+        /// <summary>
+        /// Method overload Color which doesn't request alpha value since it's
+        /// set at 255 (full opacity).
+        /// </summary>
+        /// <param name="r">stands for "red"</param>
+        /// <param name="g">stands for "green"</param>
+        /// <param name="b">stands for "blue"</param>
         public Color(byte r, byte g, byte b)
         {
             _red = SetRed(r);
@@ -23,21 +41,41 @@ namespace ColorSpheres
 
         //Set
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="r">stands for "red"</param>
+        /// <returns></returns>
         private byte SetRed(byte r)
         {
             return r;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="g">stands for "green"</param>
+        /// <returns></returns>
         private byte SetGreen(byte g)
         {
             return g;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="b">stands for "blue"</param>
+        /// <returns></returns>
         private byte SetBlue(byte b)
         {
             return b;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a">stands for "alpha"</param>
+        /// <returns></returns>
         private byte SetAlpha(byte a)
         {
             return a;
